854bf4361dd12de6074428eb569280d62ca00701
prodgers <prodgers@DESKTOP-2M3NFKM>
2016-09-14 20:56:15 -0400

    Add project files.

---
+++ Tango Down/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tango_Down
+{
+
+    public partial class MainWindow : Window
+    {
+        game thisgame = new game();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            lbl_servercount.DataContext = thisgame;
+        }
+
+        private void img_server_mousedown(object sender, MouseButtonEventArgs e)
+        {
+            thisgame.int_servercount++;
+            thisgame.str_servercount = thisgame.int_servercount + " Servers Taken Down";
+        }
+
+    }
+}
---
+++ Tango Down/game.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tango_Down
+{
+    class game : INotifyPropertyChanged
+    {
+        int _int_servercount;
+        public int int_servercount
+        {
+            get { return _int_servercount; }
+            set { _int_servercount = value; OnPropertyChanged("int_servercount"); }
+        }
+
+        string _str_servercount;
+        public string str_servercount
+        {
+            get { return _str_servercount; }
+            set { _str_servercount = value; OnPropertyChanged("str_servercount"); }
+        }
+
+        protected virtual void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public game()
+        {
+            int_servercount = 0;
+            str_servercount = int_servercount + " Servers Taken Down";
+        }
+    }
+}
